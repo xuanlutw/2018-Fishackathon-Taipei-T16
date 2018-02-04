129381afa1d6638b2bc9ea9c048841d17ec410be
b05202043 <b05202043@linux2.csie.ntu.edu.tw>
2018-02-04 09:58:10 +0800

    add logo <3

---
+++ logo.js
@@ -0,0 +1,4 @@
+var fish = document.createElement("IMG");     
+fish.src = "png/swimming.gif";
+fish.style = "position:fixed; bottom:40px; right:40px; z-index:1000; width:100px"
+document.body.appendChild(fish);
